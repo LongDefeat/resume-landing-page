22307313c6eb56bb6b5abfe53cb60c0428ddb056
LongDefeat <masonsams7@gmail.com>
2022-12-01 18:37:28 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Resume from "./Pages/Resume";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Schemas/Data.js
@@ -0,0 +1,111 @@
+export const Data = {
+    profile: {
+      name: "Mason Sams",
+      ocupation: "FullStack Software Developer",
+      location: "Pasadena, California",
+      email: "masonsams7@gmail.com",
+      telephone: "1 (479) 970-1562",
+      image: "images/profile-pic.jpg",
+    },
+    aboutMe: {
+      label: "Profile",
+      description:
+        "Product-minded, customer-focused, software engineer specializing in full-stack development. Seeking to join fast-paced engineering team, passionate about engineering exemplary user experiences and creating useful products for users to visit over and over again.",
+    },
+    skills: {
+      technicalLabel: "Technologies",
+      softLabel: "Skills",
+      technicalSkills: [
+        "React",
+        "Javascript",
+        "Node.js",
+        "Express.js",
+        "Python",
+        "Flask",
+        "jQuery",
+        "Bootstrap/React-Bootstrap",
+        "Jest",
+        "PostgreSQL",
+        "Git",
+      ],
+      softSkills: [
+        "Problem solving",
+        "Creative",
+        "Proactive",
+        "Lifelong learner",
+        "Communication",
+        "Teamwork",
+        "Teambuilding",
+      ],
+    },
+    socialMedia: {
+      label: "SOCIAL",
+      social: [
+        {
+          label: "LinkedIn",
+          name: "linkedin",
+          url: "https://www.linkedin.com/in/masonsams3945/",
+          className: "bxl-linkedin-square",
+        },
+        {
+          label: "GitHub",
+          name: "github",
+          url: "https://github.com/LongDefeat",
+          className: "bxl-github",
+        },
+      ],
+    },
+    experience: {
+      works: [
+        {
+          title: "Software Engineering Student",
+          period: "Aug. 2021 - Nov. 2022",
+          company: "Springboard Fullstack Engineering Bootcamp",
+          description: [
+            "Completed 800-hour course that covered key aspects of front-end web development, back-end web development, databases, and data structures and algorithms. Included 1:1 industry expert mentor oversight, and completion of in-depth portfolio projects..",
+            "Honed skills in Web Development, JavaScript, DOM Manipulation, Event Driven Programming, Git/Terminal/Github, Modern JavaScript and Testing, AJAX, jQuery, ReactJS, SQL, PostgresQL, Flask, Node, Express, Data Structures and Algorithms.",
+            "Assisted other students with troubleshooting their personal projects."
+          ],
+        },
+        {
+          title: "Paralegal",
+          period: "Sep. 2021 - Present",
+          company: "Lagerlof, LLP",
+          description: [
+            "Drafted estate planning documents for a variety of situations, including trusts, wills, powers of attorney, and ancillary documents.",
+            "Experience with HotDocs, LexisNexis, Wealth Counsel, Worldox, and Amicus.",
+            "Assist attorneys through Trust Administration procedures through client-facing projects and court filings."
+          ],
+        },
+      ],
+      academic: [
+        {
+          career: "History and German Language",
+          date: "2009 - 2013",
+          institution: "University of Arkanasas - Fayetteville, AR",
+        },
+      ],
+      projects: [
+        {
+          name: "Cocktail Recipe Saver",
+          description: [
+            "Created cocktail recipe website using an existing cocktail API that uses authentication for users to create profiles and save recipes.",
+            "Implemented RESTful backend to manage user authentication.",
+            "Constructed Flask in frontend and Python for backend communcations.",
+            "TechStack: Python, Bootstrap, Flask, Axios, WTForms, and PostgreSQL",
+          ],
+        },
+        {
+          name: "The Movie Maker Journal",
+          description: [
+            "Built a blogging platform based on Letterbox'd and Goodreads using authentication that allows users to create and join the app.",
+            "Implemented a RESTful backend to manage user authentication.",
+            "Used React components to build the frontend and node.js for backend functionality.",
+            "Used the Movie Database API for films and affiliated film information.",
+            "Organized backend based on specific functionality of database routes.",
+            "Techstack: Node.js, Express, React, Axios, jsonwebtoken, uuid, PostgreSQL, React-Bootstrap, bcrypt",
+          ],
+        },
+      ],
+    },
+  };
